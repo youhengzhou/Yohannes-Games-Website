168e4047ef69a84b266af7dab1f8ef6174dfd7a5
youhengzhou <youhengzhou@cmail.carleton.ca>
2024-04-28 00:38:57 -0400

    fix: remove debug values on pop

--- apps/simpleworld2/game.js
+++ apps/simpleworld2/game.js
@@ -120,7 +120,7 @@ function getWorld(rows, cols) {
       worldMap[x][y].char.name = person().name;
       worldMap[x][y].char.trait = person().trait;
       worldMap[x][y].char.atrs = {
-        adm: Math.floor(Math.random() * 6) + 100,
+        adm: Math.floor(Math.random() * 6) + 1,
         pop: Math.floor(Math.random() * 6) + 1,
         mrl: Math.floor(Math.random() * 6) + 1,
         prd: Math.floor(Math.random() * 6) + 1,
